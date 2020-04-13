9a2755e3639db9302e9aa6bc0f5d52d156b2fee8
Pascual Pérez <pascual@genuitec.com>
2020-04-13 13:11:34 -0500

    Clean up the default layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    Home
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
+  data() {
+    return {};
+  },
   name: "Home",
-  components: {
-    HelloWorld
-  }
+  components: {}
 };
 </script>
+
+<style>
+</style>
