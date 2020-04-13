beaae56f831faf794e819aa8d58dd3cdc76843bd
Pascual Pérez <pascual@genuitec.com>
2020-04-13 13:14:39 -0500

    Add Font Awesome icons

--- src/main.js
+++ src/main.js
@@ -3,6 +3,15 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./assets/tailwind.css";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faTrashAlt } from "@fortawesome/free-regular-svg-icons";
+import { faEdit } from "@fortawesome/free-regular-svg-icons";
+
+library.add(faTrashAlt);
+library.add(faEdit);
+
+Vue.component("font-awesome-icon", FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
